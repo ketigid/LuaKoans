fd11c5e6cb1d44507d8cf64dd218e3ae1a3c7f6a
ketigid <ketigid@gmail.com>
2017-03-05 14:51:40 +0800

    Completed asserts.lua and variables.lua

--- missions/asserts.lua
+++ missions/asserts.lua
@@ -1,27 +1,27 @@
 function test_assert()
-  assert_true(false) -- this should be true. This is how you write comments in Lua
+  assert_true(true) -- this should be true. This is how you write comments in Lua
 end
 
 function test_assert_with_message()
-  assert_true(false, 'This should be true')
+  assert_true(true, 'This should be true')
 end
 
 function test_assert_expression()
-  local expected_value = __
+  local expected_value = 2
   local actual_value = 1 + 1
   assert_true(expected_value == actual_value)
 end
 
 function test_assert_equals()
-  local expected_value = __
+  local expected_value = 2
   local actual_value = 1 + 1
   assert_equal(expected_value, actual_value)
 end
 
 function test_assert_fill_in_values()
-  assert_equal(1 + 1, __)
+  assert_equal(1 + 1, 2)
 end
 
 function test_assert_not()
-  assert_not(__, 'This should be false')
+  assert_not(false, 'This should be false')
 end
