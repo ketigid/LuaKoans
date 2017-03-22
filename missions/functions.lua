4cda571b7c71b8ee638da96fdee7504aeae121ed
ketigid <ketigid@gmail.com>
2017-03-22 18:53:59 +0800

    Completed functions.lua

--- missions/functions.lua
+++ missions/functions.lua
@@ -2,14 +2,14 @@ function test_local_function_inside_another_function()
   local function foo()
     return 'foo'
   end
-  assert_equal(__, foo())
+  assert_equal('foo', foo())
 end
 
 function test_local_function_with_parameters()
   local function sum(x,y)
     return x + y
   end
-  assert_equal(__, sum(6,4))
+  assert_equal(10, sum(6,4))
 end
 
 function test_assign_function_to_variable()
@@ -17,7 +17,7 @@ function test_assign_function_to_variable()
     return x + y
   end
   local f = sum
-  assert_equal(__, f(3,7))
+  assert_equal(10, f(3,7))
 end
 
 function test_assign_anonymous_function_to_variable()
@@ -25,12 +25,12 @@ function test_assign_anonymous_function_to_variable()
   local f = function(x,y)
     return x + y
   end
-  assert_equal(__, f(5,5))
+  assert_equal(10, f(5,5))
 end
 
 function test_functions_that_do_not_return_anything_return_nil()
   local f = function() end
-  assert_equal(__, f())
+  assert_equal(nil, f())
 end
 
 function test_recursive_functions()
@@ -38,7 +38,7 @@ function test_recursive_functions()
     if x <= 0 then return 0 end
     return x + recurse(x-1)
   end
-  assert_equal(__, recurse(10))
+  assert_equal(55, recurse(10))
 end
 
 function test_return_multiple_values()
@@ -47,9 +47,9 @@ function test_return_multiple_values()
   end
 
   local a,b,c = mangle(1,2,3)
-  assert_equal(__, a)
-  assert_equal(__, b)
-  assert_equal(__, c)
+  assert_equal(2, a)
+  assert_equal(3, b)
+  assert_equal(2, c)
 end
 
 function test_ignore_returned_values_on_assignments()
@@ -57,7 +57,7 @@ function test_ignore_returned_values_on_assignments()
     return 1,2,3,4,5,6
   end
   local _,_,x = stuff()
-  assert_equal(__, x)
+  assert_equal(3, x)
 end
 
 function test_use_returned_values_on_functions()
@@ -67,7 +67,7 @@ function test_use_returned_values_on_functions()
   local function sum(x,y)
     return x + y
   end
-  assert_equal(__, sum(repeat_parameter(5)))
+  assert_equal(10, sum(repeat_parameter(5)))
 end
 
 function test_ignore_returned_values_as_parameters()
@@ -77,7 +77,7 @@ function test_ignore_returned_values_as_parameters()
   local function sum(x,y)
     return x + y
   end
-  assert_equal(__, sum(stuff()))
+  assert_equal(3, sum(stuff()))
 end
 
 function test_only_the_last_invoked_function_returns_all_values_the_rest_return_just_one()
@@ -87,12 +87,12 @@ function test_only_the_last_invoked_function_returns_all_values_the_rest_return_
   local function sum(a,b,c,d)
     return a + b + c + d
   end
-  assert_equal(__, sum(numbers(), numbers()))
+  assert_equal(37, sum(numbers(), numbers()))
 end
 
 function test_parameters_not_passed_are_nil()
   local function unused_param(p)
-    assert_equal(__, p)
+    assert_equal(nil, p)
   end
   unused_param()
 end
@@ -102,7 +102,7 @@ function test_make_use_of_nil_for_default_values()
     amount = amount or 1 -- very common language idiom for default values
     return number + amount
   end
-  assert_equal(__, add(10))
+  assert_equal(11, add(10))
 end
 
 function test_functions_can_access_variables_on_their_defining_scope()
@@ -112,7 +112,7 @@ function test_functions_can_access_variables_on_their_defining_scope()
     value = 20
   end
   change()
-  assert_equal(__, value)
+  assert_equal(20, value)
 end
 
 function test_parenthesis_are_not_needed_on_invocation_when_the_only_parameter_is_a_string()
@@ -121,7 +121,7 @@ function test_parenthesis_are_not_needed_on_invocation_when_the_only_parameter_i
     str:gsub(" ", function() count = count + 1 end) -- notice the closure here!
     return count
   end
-  assert_equal(__, count_spaces "This string has four spaces") -- no parenthesis!
+  assert_equal(4, count_spaces "This string has four spaces") -- no parenthesis!
 end
 
 function test_parenthesis_are_not_needed_on_invocation_when_the_only_parameter_is_a_table()
@@ -132,7 +132,7 @@ function test_parenthesis_are_not_needed_on_invocation_when_the_only_parameter_i
     end
     return count
   end
-  assert_equal(__, count_zeroes {1, 0, 2, 3, 0, 4, 5, 6, 0, 7})
+  assert_equal(3, count_zeroes {1, 0, 2, 3, 0, 4, 5, 6, 0, 7})
 end
 
 function test_variable_number_of_arguments_with_dot_dot_dot()
@@ -140,5 +140,5 @@ function test_variable_number_of_arguments_with_dot_dot_dot()
     local _,_,x = ...
     return x
   end
-  assert_equal(__, third('a','b','c','d'))
+  assert_equal('c', third('a','b','c','d'))
 end
